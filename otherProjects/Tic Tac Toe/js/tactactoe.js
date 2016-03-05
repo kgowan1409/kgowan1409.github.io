d71b95960fc37d40974f84e0637dd172d309ec75
kgowan1409 <gowan.katrina@gmail.com>
2016-03-05 18:00:39 -0500

    Update tactactoe.js

--- otherProjects/Tic Tac Toe/js/tactactoe.js
+++ otherProjects/Tic Tac Toe/js/tactactoe.js
@@ -1,8 +1,9 @@
 $(document).ready(function() {
 
-  var circleOrEx = "o"; // this is the o symbol put in the box
-  var isGameInProgress = true; // The timeframe the game is being played
-  var winningCombos = { // the combos that will win the game and end the game
+  var circleOrEx = "o"; //the first click is O
+    
+  var isGameInProgress = true; // The game is defaulted to be in progress.
+  var winningCombos = { // The board is a combo of 9 squares and its started
     0: [ //0 is key
       [1, 2], //this multiDimensional Array is values
       [3, 6],
@@ -47,16 +48,17 @@ $(document).ready(function() {
     ]
   };
 
-  //this decides where the x or o when clicked
+  // When you click on the board, the function begins and the gameinprocess is true 
   $("#board").find("div").on("click", function() {
 
-    if (isGameInProgress && $(this).hasClass("empty")) { /// if the box is empty add a circle or ex to the end
+    if (isGameInProgress && $(this).hasClass("empty")) { // within the #board remove the empty class and add an X or an O value to the square
+
       $(this).removeClass("empty").append("<span class='" + circleOrEx + "'>" + circleOrEx + "</span");
 
-      checkIfWon($(this).index(), circleOrEx); //find out who wins by the winningcombos in the index
+      checkIfWon($(this).index(), circleOrEx); //cycle of the game
 
       if (circleOrEx === "o") {
-        circleOrEx = "x";
+        circleOrEx = "x";// this shows when your your mouse clicks on the square. It shows x then o and so forth
       } else {
         circleOrEx = "o";
       }
@@ -64,51 +66,48 @@ $(document).ready(function() {
 
   });
 
-  //this activates a new game by clicking on the button
+  // this makes a new game 
   $("#newGame").on("click", function() {
 
-    var boardSquares = $("#board").find("div"); //what is this variable
-    var firstEmptyMemorySquare = $(".container").find(".nine").filter(function() { //bonus Explain what filter does
+    var boardSquares = $("#board").find("div"); //this actionfinds the div that finds the old board square 
+    var firstEmptyMemorySquare = $(".container").find(".nine").filter(function() { //how many games have been played
       return $.trim($(this).text()) === "" && $(this).children().length === 0;
     }).not("#board").first();
 
-    if (firstEmptyMemorySquare.length == 1) { //what is this if statement doing?
+    if (firstEmptyMemorySquare.length == 1) { //this find the board html during game
       firstEmptyMemorySquare.html($("#board").html());
-    } else {
+    } else {// find old or create new board
       $(".container").find(".nine").not("#board").empty();
       $(".container").find(".nine").first().html($("#board").html());
     }
 
-    //empty the HTML while the game is in progress
+    //this starts a new game
     boardSquares.each(function() {
       $(this).addClass("empty").empty();
     })
     isGameInProgress = true;
   })
 
-  //Explain this funciton, describe the parameters; what are the possible values of the paramaters
-  //This function selects the square that wins the game with the winning combos
+  //this function checks if the player has won by checking if the numbers match the winning combos
   function checkIfWon(chosenSquare) {
 
     var mulitArr = winningCombos[chosenSquare];
     var playerWon;
 
-    for (var i = 0; i < mulitArr.length; i++) { //Explain this nested for loop
+    for (var i = 0; i < mulitArr.length; i++) { //value of i is less than the length of the multiarray, playerwon is now true
       playerWon = true;
       for (var j = 0; j < mulitArr[i].length; j++) {
-        if (!$("#board").find("div").eq(mulitArr[i][j]).find("span").hasClass(circleOrEx)) { //Explain this condition
+        if (!$("#board").find("div").eq(mulitArr[i][j]).find("span").hasClass(circleOrEx)) { //Finds the x or o through the arrays
           playerWon = false;
         }
       }
 
-      if (playerWon) { //Explain the condition and every line in the block
-
-        for (var j = 0; j < mulitArr[i].length; j++) {
-          $("#board").find("div").eq(mulitArr[i][j]).find("." + circleOrEx).addClass("green"); //Explain this condition
+      if (playerWon) { //after a player wins this makes the other lines to be affected and the game progress is set to "false" (var j = 0; j < mulitArr[i].length; j++) {
+          $("#board").find("div").eq(mulitArr[i][j]).find("." + circleOrEx).addClass("green"); //makes the winning combinatoin green 
         }
         $("#board").find("div").eq(chosenSquare).find("." + circleOrEx).addClass("green");
-        alert("Winner is " + circleOrEx.toUpperCase() + "!");
-        isGameInProgress = false;
+        alert("Winner is " + circleOrEx.toUpperCase() + "!");//alerts the winner which is either x or o
+        isGameInProgress = false;//the game isn't in progress anymore
         return false; //this exits the loop
       }
     }
