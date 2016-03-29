39339badbed6cfc5fde9e2abe7e8ee025199e481
kgowan1409 <gowan.katrina@gmail.com>
2016-03-29 09:05:41 -0400

    Update final.js

--- otherProjects/finalProject/fjs/final.js
+++ otherProjects/finalProject/fjs/final.js
@@ -8,5 +8,17 @@ $(".nav").find("li").removeClass("active");
 
 //add active class to clicked li
 $(this).addClass("active");
-})
-})
+
+var page = $(this).attr("id");
+alert(page);
+
+})//click
+function getPartial(partial) {
+  
+  alert(partial);
+  
+}
+ // 
+getPartial("homePage");
+  
+})//ready
