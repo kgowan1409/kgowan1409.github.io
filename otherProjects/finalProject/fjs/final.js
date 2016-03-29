287aee212a9b2dc410697f86f429ce13ed69df0a
kgowan1409 <gowan.katrina@gmail.com>
2016-03-29 08:53:48 -0400

    Update final.js

--- otherProjects/finalProject/fjs/final.js
+++ otherProjects/finalProject/fjs/final.js
@@ -1,5 +1,12 @@
 $(document).ready(function(){
+  
+  //get all the nav li, add click event
 $(".nav").find("li").on("click",function(){
+  
+  //remove all active class
 $(".nav").find("li").removeClass("active");
+
+//add active class to clicked li
 $(this).addClass("active");
-}
+})
+})
