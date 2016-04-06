0b88c37c3ac09e6eef194b6b73ffd3c92fd06798
kgowan1409 <gowan.katrina@gmail.com>
2016-04-06 13:31:43 -0400

    Update question4.js

--- otherProjects/ajaxExamples/js/question4.js
+++ otherProjects/ajaxExamples/js/question4.js
@@ -5,16 +5,15 @@ $(document).ready(function(){
 
                 console.dir(data);
                   var html="<table class='table table-hover table-striped'>"+
-                "<tr><th>Name</th><th>age</th><th>Company</th></tr>";
+                "<tr><th>Name</th><th>Age</th><th>Company</th></tr>";
                   $.each(data, function(index, item){
                   //  $("#data").append(item.name);
 
                 html+="<tr>"+
                 "<td>" +item.name +"</td>"+
-                "<td>"+item.phone + "</td>"+
-                "<td>"+item.email + "</td>"+
-                "<td>"+item.address + "</td>"+
-            
+                "<td>"+item.age + "</td>"+
+                "<td>"+item.company + "</td>"+
+               
                 "</tr>";
                   }) // each end
 
