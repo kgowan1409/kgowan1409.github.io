1deef6c28874def08af25651508f57e6a3bc9cc7
kgowan1409 <gowan.katrina@gmail.com>
2016-04-06 16:16:18 -0400

    Create about.js

---
+++ otherProjects/finalProject/js/about.js
@@ -0,0 +1,19 @@
+$(document).ready(function(){
+// button to change when mouse is over button
+//$("#like").on("Click", function(){
+
+// when mouse is off of button
+//.on("mouseleave", function(){
+
+    //  $(this).text("Click Me!");
+
+$("#like").on("click", function(){
+alert("Thank you for liking us on Facebok");
+
+
+
+
+
+
+})
+});
