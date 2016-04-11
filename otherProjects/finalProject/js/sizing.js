6f7e672f82b1a13f37af0b741da009f18de0496b
kgowan1409 <gowan.katrina@gmail.com>
2016-04-11 13:25:53 -0400

    Update and rename models.js to sizing.js

--- otherProjects/finalProject/js/sizing.js
+++ otherProjects/finalProject/js/sizing.js
@@ -1,20 +1,20 @@
 $(document).ready(function(){
-$("#getModels").on("click",function(){
+$("#getSizing").on("click",function(){
   console.log("click")
 
-$.getJSON("Final_JsonDatabase/Models.json" ,function(data){
+$.getJSON("fJsonDatabase/sizing.json" ,function(data){
   console.log("data"+data)
 // for the table to appear 
 var html="<table class='table table-hover table-striped'>"+
-"<tr><th>Name</th><th>age</th><th>Company</th><th>Gender</th></tr>";
+"<tr><th>Violin Size</th><th>Arm Length</th><th>Company</th><th>Approxamate Age</th></tr>";
   $.each(data, function(index, item){
   //  $("#data").append(item.name);
 
 html+="<tr>"+
-"<td>" +item.name +"</td>"+
+"<td>" +item.size +"</td>"+
+"<td>"+item.length + "</td>"+
 "<td>"+item.age + "</td>"+
-"<td>"+item.company + "</td>"+
-"<td>"+item.gender + "</td>"+
+
 "</tr>";
   }) // each end
 
