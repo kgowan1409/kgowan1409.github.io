f7f77d6fd1e162232bd323a66b1a463f2837a792
kgowan1409 <gowan.katrina@gmail.com>
2016-04-12 00:35:43 -0400

    Update finalSite.js

--- otherProjects/finalProject/js/finalSite.js
+++ otherProjects/finalProject/js/finalSite.js
@@ -1,23 +1,12 @@
 $(document).ready(function(){
-
-  $(".nav").find("li").on("click",function(){
-    $(".nav").find("li").removeClass("active");
-    $(this).addClass("active");
-  })
-})
-// nts : put and id on all the nav tabs too ____
-
-
-// Js for the Sizing Page
-$(document).ready(function(){
 $("#getSizing").on("click",function(){
   console.log("click")
 
 $.getJSON("fJsonDatabase/sizing.json" ,function(data){
   console.log("data"+data)
-// Code  for the table
+// for the table to appear 
 var html="<table class='table table-hover table-striped'>"+
-"<tr><th>Violin Size</th><th>Arm Length</th><th>Approxamate age</th></tr>";
+"<tr><th>Violin Size</th><th>Arm Length</th><th>Approxamate Age</th></tr>";
   $.each(data, function(index, item){
   //  $("#data").append(item.name);
 
@@ -25,7 +14,7 @@ html+="<tr>"+
 "<td>" +item.size +"</td>"+
 "<td>"+item.length + "</td>"+
 "<td>"+item.age + "</td>"+
-//"<td>"+item.picture + "</td>"+
+
 "</tr>";
   }) // each end
 
@@ -39,5 +28,3 @@ html+="<tr>"+
 }) // click end
 
 })// doc end
-
-// about js
