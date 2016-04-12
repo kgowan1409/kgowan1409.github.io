a699b3b96f088cc1a6e35edee6fcf36a354323c5
kgowan1409 <gowan.katrina@gmail.com>
2016-04-12 17:48:06 -0400

    Update sizing.js

--- otherProjects/finalProject/js/sizing.js
+++ otherProjects/finalProject/js/sizing.js
@@ -1,14 +1,14 @@
 $(document).ready(function(){
-$("#getSizing").on("click",function(){
+$("#getSizing").on("click",function(){//getting the button
   
   
 $.getJSON("http://kgowan1409.github.io/otherProjects/finalProject/fJsonDatabase/sizing.json", function(data){
   console.log("data"+data)
-// for the table to appear 
+// calling the JSON for the table to appear 
 var html="<table class='table table-hover table-striped'>"+
 "<tr><th>Violin Size</th><th>Arm Length</th><th>Approxamate Age</th></tr>";
   $.each(data, function(index, item){
-  //  $("#data").append(item.name);
+ 
 
 html+="<tr>"+
 "<td>" +item.size +"</td>"+
